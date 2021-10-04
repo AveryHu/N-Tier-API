8c47b8346558d304b4e019324e50c95efd2bd434
Avery Hu <dc06dcj6@yahoo.com>
2021-10-05 02:24:56 +0800

    [Database] Add Entity and Context
    Init with an entity class and local context.

---
+++ WebAPI.DataLayer/DatabaseContext/LocalContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebAPI.Domain.Entities;
+
+namespace WebAPI.DataLayer.DatabaseContext
+{
+    public class LocalContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        public LocalContext(DbContextOptions<LocalContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ WebAPI.Domain/Entities/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebAPI.Domain.Enum;
+
+namespace WebAPI.Domain.Entities
+{
+    public class Account
+    {
+		public int Identity { get; set; }
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string Password { get; set; }
+		public DateTime CreationDate { get; set; }
+		public Gender UserGender { get; set; }
+	}
+}
