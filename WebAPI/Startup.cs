8e5698cd4843feeb3a2bb8ca5dbcfb9d4ad94874
Avery Hu <dc06dcj6@yahoo.com>
2021-10-04 18:08:17 +0800

    [BugFix] Fix logic injection not init
    Fix the logic injection does not register in startup

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -27,6 +27,7 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            AddLogics(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
